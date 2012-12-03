05fcc794ea3f73e59bab8843d1f969b2023a70ba
Malte Engelhardt <kurvenschubser@gmail.com>
2012-12-03 17:55:01 +0100

    reverting to master for creating new branch

--- src/hx_seq.h
+++ src/hx_seq.h
@@ -9,62 +9,138 @@
 #include <stdarg.h>
 
 
-
 typedef struct HxSeqElem_
 {
 	struct HxSeqElem_ *next;
 	void *obj;
-} HxSeqElem;
+} hx_seq_elem_t;
 
 
 typedef struct HxSeq_
 {
-	HxSeqElem *first;
+	hx_seq_elem_t *first;
 	long length;
-} HxSeq;
+} hx_seq_t;
 
 
 typedef struct HxSeqIter_
 {
-	HxSeq *seq;
-	HxSeqElem *current;
+	hx_seq_t *seq;
+	hx_seq_elem_t *current;
 	bool initiated;
-} HxSeqIter;
+} hx_seq_iter_t;
 
 
-HxSeqIter *
-HxSeqIter_new(HxSeq *seq);
-
-bool HxSeqIter_next(HxSeqIter *self);
-
-void
-HxSeqIter_rewind(HxSeqIter *self);
-
-void *
-HxSeqIter_get(HxSeqIter *self);
-
-void
-HxSeqIter_del(HxSeqIter *self);
-
-HxSeq *
+hx_seq_t *
 HxSeq_new(void *first, ...);
 
-void
-HxSeq_del(HxSeq *self);
 
 void
-HxSeq_append(HxSeq *self, void *obj);
+HxSeq_del(hx_seq_t *self);
+
+
+void
+HxSeq_append(hx_seq_t *self, void *obj);
+
+
+/*
+ * @return A new sequence made up of all elements of *self* and *other*.
+ */
+hx_seq_t *
+HxSeq_add(hx_seq_t *self, hx_seq_t *other);
+
+
+/*
+ * In-place adding off all elements from other to *self*.
+ */
+void
+HxSeq_extend(hx_seq_t *self, hx_seq_t *other);
+
+
+void
+HxSeq_extend_base(hx_seq_t *self, bool before, long index, hx_seq_t *other);
+
+
+hx_seq_t *
+HxSeq_clone(hx_seq_t *self);
+
 
 void *
-HxSeq_pop(HxSeq *self, long index);
+HxSeq_pop(hx_seq_t *self, long index);
+
 
 long
-HxSeq_len(HxSeq *self);
+HxSeq_len(hx_seq_t *self);
 
-HxSeqElem *
-HxSeqElem_new(void *obj, void *next);
+
+long
+HxSeq_index(hx_seq_t *self, void *obj);
+
+
+void *
+HxSeq_get_item(hx_seq_t *self, long index);
+
+
+hx_seq_elem_t *
+HxSeq_get_elem(hx_seq_t *self, long index);
+
+
+void HxSeq_check_index(hx_seq_t *self, long index);
+
 
 void
-HxSeqElem_del(HxSeqElem *self);
+HxSeq_sort(hx_seq_t *self, void *((* algo_fn)(hx_seq_t *sortee, int (* cmp)(void *, void *))), int (* comp_fn)(void *, void *));
+
+
+hx_seq_t *
+HxSeq_sorted(hx_seq_t *self);
+
+
+void
+HxSeq_reverse(hx_seq_t *self);
+
+
+hx_seq_t *
+HxSeq_filter(hx_seq_t *self, bool (*filter)(void *));
+
+
+hx_seq_t *
+HxSeq_slice(hx_seq_t *self, long start, long stop, long step);
+
+
+hx_seq_elem_t *
+HxSeqElem_new(void *obj, void *next);
+
+
+void
+HxSeqElem_del(hx_seq_elem_t *self);
+
+
+hx_seq_elem_t *
+HxSeqElem_clone(hx_seq_elem_t *self);
+
+
+hx_seq_iter_t *
+HxSeqIter_new(hx_seq_t *seq);
+
+
+bool
+HxSeqIter_next(hx_seq_iter_t *self);
+
+
+void
+HxSeqIter_rewind(hx_seq_iter_t *self);
+
+
+void *
+HxSeqIter_get(hx_seq_iter_t *self);
+
+
+hx_seq_elem_t *
+HxSeqIter_get_elem(hx_seq_iter_t *self);
+
+
+void
+HxSeqIter_del(hx_seq_iter_t *self);
 
 #endif		/* HX_SEQ_H */
